cb20369200b7899db6bf9b011f1cc3b5e798c0ca
Agustin Bergomi <agustin.bergomi@gmail.com>
2020-12-10 17:33:57 -0300

    Primer commit

---
+++ app/src/main/java/com/agustinbergomi/actionbuttons/MainActivity.java
@@ -0,0 +1,111 @@
+package com.agustinbergomi.actionbuttons.;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.ClipData;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view,
+                        "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+//getMenuInflater().inflate(R.menu.menu_main, menu);
+        CreateMenu(menu);
+        return true;
+    }
+    private void CreateMenu(Menu menu)
+    {
+        MenuItem mnu1 = menu.add(0, 0, 0, "Item 1");
+        {
+            mnu1.setIcon(R.mipmap.ic_launcher);
+            mnu1.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        }
+        MenuItem mnu2 = menu.add(0, 1, 1, "Item 2");
+        {
+            mnu2.setIcon(R.mipmap.ic_launcher);
+
+            mnu2.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        }
+        MenuItem mnu3 = menu.add(0, 2, 2, "Item 3");
+        {
+            mnu3.setIcon(R.mipmap.ic_launcher);
+            mnu3.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        }
+        MenuItem mnu4 = menu.add(0, 3, 3, "Item 4");
+        {
+            mnu4.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        }
+        MenuItem mnu5 = menu.add(0, 4, 4,"Item 5");
+        {
+            mnu5.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        }
+    }
+    private boolean MenuChoice(MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case 0:
+                Toast.makeText(this, "You clicked on
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case 1:
+                Toast.makeText(this, "You clicked on
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case 2:
+                Toast.makeText(this, "You clicked on
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case 3:
+                Toast.makeText(this, "You clicked on
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case 4:
+                Toast.makeText(this, "You clicked on
+                        Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return false;
+    }
+    ClipData.Item 1",
+    ClipData.Item 2",
+    ClipData.Item 3",
+    ClipData.Item 4",
+    ClipData.Item 5",
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+// Handle action bar item clicks here. The action bar will
+// automatically handle clicks on the Home/Up button, so long
+// as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+//noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return MenuChoice(item);
+    }
+}
